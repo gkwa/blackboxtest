d1d0cc73655918fd496ded31b7dc0be4411671fd
Taylor Monacelli <taylormonacelli@gmail.com>
2015-04-12 22:16:25 -0700

    yada-yada

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # blackboxtest
 
 Test out this tool for keeping secrets in VCS: https://github.com/StackExchange/blackbox
+
+
+https://www.gnupg.org/documentation/manuals/gnupg/Unattended-GPG-key-generation.html
+
+https://github.com/StackExchange/blackbox
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat >foo <<EOF
+%echo Generating a basic OpenPGP key
+Key-Type: DSA
+Key-Length: 1024
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: Taylor Monacelli
+Name-Comment: with stupid passphrase
+Name-Email: tailor@u.washington.edu
+Expire-Date: 2
+Passphrase: abc
+%pubring foo.pub
+%secring foo.sec
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
+EOF
+
+gpg2 --batch --gen-key foo
+gpg2 --no-default-keyring --secret-keyring ./foo.sec --keyring ./foo.pub --list-secret-keys
